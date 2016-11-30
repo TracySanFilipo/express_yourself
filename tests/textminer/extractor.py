ec39805c7aecc330f45908fb9eb8c8fe3deee58a
TracySanFilipo <tracysanfilipo@gmail.com>
2016-11-30 12:47:08 -0500

    Added __init__ files

--- tests/textminer/extractor.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-def phone_numbers(document_to_search):
-    return re.findall(r"\(?\d{3}\)?\s?\-?\d{3}\s?\-?\d{4}", document_to_search)
-
-
-def emails(document_to_search):
-    return re.findall(r"\w+\.*\w*\@\w+\.\w+", document_to_search)
